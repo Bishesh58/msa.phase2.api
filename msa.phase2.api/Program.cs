309293198bea39da8e15bd0ca7136ad94167d5ad
Bishesh58 <bishesh.sunam@gmail.com>
2022-08-08 17:59:24 +1200

    Add: default data inMemory db Context

--- msa.phase2.api/Program.cs
+++ msa.phase2.api/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using msa.phase2.api.Models;
+using msa.phase2.api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,9 @@ builder.Services.AddControllers();
 //register dbContext Product
 builder.Services.AddDbContext<ProductContext>(opt => opt.UseInMemoryDatabase("Products"));
 
+
+//service injection
+//builder.Services.AddScoped <IProductService Product >();
 
 //add fakestore api client
 builder.Services.AddHttpClient("FakeStore", configureClient: client =>
@@ -25,6 +29,9 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+//helper method call to add data into db
+AddProductData(app);
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -39,3 +46,26 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+//helper method to add some data into inMemory db
+
+static void AddProductData(WebApplication app)
+{
+    var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetService<ProductContext>();
+
+    var product1 = new Product { Id = 1, Description = "Men's T-shirt", ImageUrl = "men_tshirt1.png", Price = 120, Title = "T-shirt 1" };
+    var product2 = new Product { Id = 2, Description = "Men's T-shirt", ImageUrl = "men_tshirt2.png", Price = 20, Title = "T-shirt 2" };
+    var product3 = new Product { Id = 3, Description = "Men's T-shirt", ImageUrl = "men_tshirt3.png", Price = 130, Title = "T-shirt 3" };
+    var product4 = new Product { Id = 4, Description = "Men's T-shirt", ImageUrl = "men_tshirt4.png", Price = 150, Title = "T-shirt 4" };
+    var product5 = new Product { Id = 5, Description = "Men's T-shirt", ImageUrl = "men_tshirt5.png", Price = 110, Title = "T-shirt 5" };
+
+    db.Products.Add(product1);
+    db.Products.Add(product2);
+    db.Products.Add(product3);
+    db.Products.Add(product4);
+    db.Products.Add(product5);
+
+    db.SaveChanges();
+
+}
